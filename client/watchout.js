20ff08469bb95a8a63bc3853f21388a9ee1f5445
conniedaisy <conniedcheung@gmail.com>
2016-03-11 15:51:45 -0800

    Implement enemy position generator

--- client/watchout.js
+++ client/watchout.js
@@ -16,5 +16,34 @@
 //update span element for score
 
 
-//create an object with random positions
-//add the 
+//create an array with random positions
+//position ranges: 
+
+//append to svg
+
+// create an array of position objects
+// for with loop with 30 iterations
+  // Math.random() * width
+  // Math.random() * SVGheight
+  // set those as properties of the newly created object
+  // push to storage array
+
+
+var SVGwidth = 700;
+var SVGheight = 450;
+
+var generatePositions = function(num) {// num is number of enemies to generate
+  var enemyPositions = [];
+  for (var i = 0; i < num; i++) {
+    var enemyPosition = {};
+    var x = Math.random() * SVGwidth;
+    var y = Math.random() * SVGheight;
+    enemyPosition['x'] = x;
+    enemyPosition['y'] = y;
+    enemyPositions.push(enemyPosition);
+  }
+};
+
+d3.select("svg")
+.data([])
+.enter().append(<span class: enemy>).style(top, function(d){}).style(left, function(d){})
