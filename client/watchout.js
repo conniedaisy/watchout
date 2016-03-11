0d7e5ccca878362db4e966412b8523b3397f325c
conniedaisy <conniedcheung@gmail.com>
2016-03-11 15:25:14 -0800

    Implement initial styling for board

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,20 @@
 // start slingin' some d3 here.
+
+
+//location data
+//[{x, y}, {x1, y1}]
+//pass through D3 data binding
+//use style method to update the nodes
+  // function(d) {
+    // return 'top: '+d.x +'right: '+d.y
+  //}
+//use transition method
+
+// user click, if statement, if click === player position, then update position according to mouse position
+// d3.behavior.drag
+
+//update span element for score
+
+
+//create an object with random positions
+//add the 
